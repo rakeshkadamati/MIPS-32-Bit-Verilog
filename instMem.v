60c574892f29a7337ee1fccf0139b692a9219b0b
sxc125530 <chang.stephen12@gmail.com>
2016-04-30 20:19:50 -0500

    Added files via upload
    Changed instructions back to original from karthik's github

--- instMem.v
+++ instMem.v
@@ -4,28 +4,26 @@ input [31:0] pc;
 output [31:0] inst;
 reg [31:0] inst;
 
-//128 element array of 32 bit instructions
 reg [31:0] memdata [127:0];
 
 initial
         begin
+        //128 instructions to be added here, each of 32 bits, but for sake of simplicity, only added 7 instructions
 
-        memdata[0] = 32'h20070003;
-        memdata[1] = 32'h20030009;
-        memdata[2] = 32'h20060006;
-        memdata[3] = 32'h00672822;
-        memdata[4] = 32'h10a70000;
-        memdata[5] = 32'h01095024;
-        memdata[6] = 32'h8c020000;
-        //memdata[7] = 32'h;
-        //memdata[8] = 32'h;
-        //memdata[9] = 32'h;
+        memdata[0] = 32'b00000000000000010001000000100010;
+        memdata[1] = 32'b00000000001000100001100000100100;
+        memdata[2] = 32'b00000000010000010011000000100101;
+        memdata[3] = 32'b00000001100010010101000000100000;
+        memdata[4] = 32'b10001100000000010001000000100000;
+        memdata[5] = 32'b10101100000000010001000000100000;
+        memdata[6] = 32'b00001000000000010001000000100000;
+        //memdata[7] = 32'b00000000000000010001000000100000;
+        //memdata[8] = 32'b00000000000000010001000000100000;
+        //memdata[9] = 32'b00000000000000010001000000100000;
 
-
-end
+        end
 
 always @(posedge clk)
-//set instruction to new pc
         begin
                 inst = memdata[pc];
         end
